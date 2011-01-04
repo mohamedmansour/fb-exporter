460a0409987182aec6d2a918002c34e6a29b8834
Mohamed Mansour <hello@mohamedmansour.com>
2011-01-03 21:58:15 -0500

    Extract all friends implementation, taps into Facebooks JavaScript Framework. Somehow Facebook removed the usage of text based emails, they are now placing the emails as an image hmm.

--- js/fb-exporter.js
+++ js/fb-exporter.js
@@ -1,11 +1,160 @@
-var work_tab_id = 0;
+var friendsMap = {};
+
+// Just draw the export friends link on the top next to the other links.
+renderExportFriendsLink();
+
+// Listen on extension requests.
+chrome.extension.onRequest.addListener(function(request, sender, sendResponse) {
+  if (request.startExportFriendData) {
+    startExportFriendData();
+  }
+  else if (request.getFriendsMap) {
+    sendResponse({data: friendsMap});
+  }
+});
+
+
+function switchToWorkerTab() {
+  // The extension will handle the case if the worker tab already exists.
+  chrome.extension.sendRequest({switchToWorkerTab: 1}, function(response) {
+    console.log('Returned from extension');
+  });
+}
+
+function createFacebookDialog(data) {
+   // Creates the confirm component.
+   var lblConfirm = $('<label class="uiButton uiButtonLarge uiButtonConfirm">');   
+   $('<input>', {
+      type: 'button',
+      val: data.yes_text,
+      click: data.yes_callback
+   }).appendTo(lblConfirm);
+   
+   // Creates the cancel component.
+   var lblCancel = $('<label class="uiButton uiButtonLarge">');
+   $('<input>', {
+      type: 'button',
+      val: 'Cancel',
+      click: data.cancel_callback
+   }).appendTo(lblCancel);
+   
+   // Wrap the buttons to a container.
+   var dialogButtons = $('<div class="dialog_buttons clearfix">');
+   lblConfirm.appendTo(dialogButtons);
+   lblCancel.appendTo(dialogButtons);
+   
+   // Main contents for the dialog description.
+   var dialogContent = $('<div class="dialog_content">');
+   $('<div class="dialog_body">' +
+     '  <form id="report_form" onsubmit="return false;">' +
+     '    <div class="pam uiBoxWhite noborder">' +
+     '      <div class="pbm">' +
+     '        ' + data.message +
+     '      </div>' +
+     '    </div>' +
+     '  </form>' +
+     '</div>').appendTo(dialogContent);
+   dialogButtons.appendTo(dialogContent);
+   
+   // Add the title to the the dialog and the contents as well.
+   var dialog = document.createElement('div');
+   $(dialog).addClass('pop_container_advanced')
+            .attr('id', data.id)
+            .css('position', 'absolute')
+            .css("left", "25%")
+            .css("right", "25%")
+            .css("top", "20%")
+            .css("zIndex", "999999");
+   var dialogPopup = $('<div class="pop_content" id="pop_content">');
+   $('<h2 class="dialog_title"><span>' + data.title + '</span></h2>').appendTo(dialogPopup);
+   dialogContent.appendTo(dialogPopup);
+   dialogPopup.appendTo(dialog);
+   return dialog;
+}
+
+/**
+ * To transport JavaScript data from Facebook, it is faster and better to transfer
+ * the internal map Facebook maintains. To do that, we create a transfer dom area
+ * to the page so we dump the FriendSearchPane and then store it in the background
+ * page right afterwards.
+ */
+function exportFacebookContacts() {
+  // JS script injection to the facebook's World.
+  var postFriendMap = function() {
+    var textarea = document.getElementById('transfer-dom-area');
+    textarea.value = JSON.stringify(FriendSearchPane._data);
+  };
+  
+  // Create a dummy textarea DOM.
+  var textarea = document.createElement('textarea');
+  $(textarea).attr('id', 'transfer-dom-area')
+             .hide()
+             .appendTo($(document.body));
+  
+  // Start injecting the JS script.
+  var script = document.createElement('script');
+  script.appendChild(document.createTextNode('(' + postFriendMap + ')();'));
+  document.body.appendChild(script);
+  
+  // Inform our world that we have received the friend map data.
+  friendsMap = JSON.parse(textarea.value);
+  
+  // Clean up since we no longer need this.
+  $(textarea).remove();
+}
+
+/**
+ * To make sure the user wants to start exporting, we need to prompt her to
+ * see if it's OK to go to the right page (she might have some input in an
+ * input field that is not saved, etc).
+ */
+function goToFriendPageAndStart() {
+  // See if we are at the right page to start.  We need to be at the /friends/*
+  // location, to get access to the list of friends. Any other page won't do.
+  if (document.location.pathname.match('^/friends/edit') && document.location.search == 0) {
+    exportFacebookContacts();
+    switchToWorkerTab();
+  }
+  else {     
+    $(document.body).append(createFacebookDialog({
+      id: 'fb-exporter-redirect',
+      title: 'Redirection needed',
+      message: 'First, you need to go to your friends page, do you want us to redirect you to it?',
+      yes_text: 'Redirect now',
+      yes_callback: (function() {
+        $('#fb-exporter-redirect').remove();
+        switchToWorkerTab();
+        window.location = 'http://www.facebook.com/friends/edit';
+      }),
+      cancel_callback: (function() {
+        $('#fb-exporter-redirect').remove();
+      })
+    }));
+  }
+}
+
+/**
+ * Adds a export friends link to the top of the worker tab.
+ */
+function renderExportFriendsLink() {
+  // Paint the Export friends to the top of the page.
+  var exportFriendsLink = $('#pageNav a:contains("Home")').parent().clone();
+  $('a', exportFriendsLink)
+      .attr('id', 'export-friends-link')
+      .attr('href', 'javascript:void(0);')
+      .text('Export friends!')
+      .css('color', 'white')
+      .click(goToFriendPageAndStart);
+  exportFriendsLink.appendTo($('#pageNav a:contains("Home")').parent());
+}
+
+
 
 function friendInfoIframeLoaded() {
-
   console.log("iframe loaded " + this.src);
-
+  
   var iframe = this;
-
+  
   // We can't access the iframe elements directly, because they probably have
   // not loaded yet.  The contact information fields are appended dynamically.
   // Instead, we use setTimeout to give all the pagelets a chance to load.
@@ -65,34 +214,19 @@ function friendInfoIframeLoaded() {
       return $(this).text();
     }).get();
 
-    chrome.extension.sendRequest({relayInfoForFriend: friend}, function(response) {
-      console.log("work tab received friend");
-    });
+    //chrome.extension.sendRequest({relayInfoForFriend: friend}, function(response) {
+    //  console.log("work tab received friend");
+    //});
 
   }, 3000, this);
 
-  //$(iframe).remove();
-}
-
-function hideTopBanner() {
-  $(".fb-exporter-top-banner").hide();
+  $(iframe).remove();
 }
 
 function startExportFriendData() {
-  $(".fb-exporter-top-banner").show();
-
-  // Remove existing iframes.
-  $("iframe.fb-exporter").remove();
-
-  // NOTE TO FUTURE SELF: If this extension breaks, it's probably because the
-  // following lines no longer work.  Namely, the fragile selector access being
-  // done below to get at the corresponding fields might need to be updated.
-  // Look at the actual FB page in question to make the right fixes.  PS: Who
-  // wins the next 20 world series??
-  var container = document.querySelectorAll("#editFriendsSearchContainer");
-  $("li:visible > div > a", $(container)).each( function(idx, e) {
-    var href = $(e).attr("href");
-
+  var i = 0;
+  $.each(friendsMap, function(key, value) {
+    var href = 'http://www.facebook.com' + value.path;
     if (href.match("\\?")) {
       href += "&v=info";
     } else {
@@ -105,185 +239,12 @@ function startExportFriendData() {
     setTimeout(function() {
       var iframe = document.createElement("iframe");
       $(iframe).attr("src", href)
-               .attr("class", "fb-exporter")
-               .css("visibility", "hidden")
-               .css("display", "none")
-               ;
+               .attr("class", "fb-exporter");
 
       $(document.body).prepend(iframe);
       $(iframe).load(friendInfoIframeLoaded);
-    }, idx * 11000 + Math.random() * 1000);
+    }, i * 11000 + Math.random() * 1000);
+    
+    i++;
   });
 }
-
-function switchToWorkerTab() {
-  console.log("switchToWorkerTab");
-
-  // The extension will handle the case if the worker tab already exists.
-  chrome.extension.sendRequest({switchToWorkerTab: 1}, function(response) {
-    console.log("returned from extension");
-  });
-}
-
-function getFriendList() {
-  console.log("getFriendList");
-  // NOTE TO FUTURE SELF: If this extension breaks, it's probably because the
-  // following lines no longer work.  Namely, the fragile selector search being
-  // done below might need to be tweaked, if the page is laid out differently.
-  var container = document.querySelectorAll("#editFriendsSearchContainer");
-  var ret = $('li:visible div > div > div a:not(:empty)', $(container)).map(function(idx, el) {
-    console.log(el);
-    return $(el).text();
-  }).get();
-  console.log("returning");
-  console.log(ret);
-  return ret;
-}
-
-/**
- * Imitiate Facebook's dialog UI.
- * @param {string} title The title of the dialog.
- * @param {string} msg The main message.
- * @param {string} btnConfirmName The name of the confirm button.
- * @param {Function} confirmCallback The callback when confirm has been clicked.
- * @param {Function} cancelCallback The callback when cancel has been clicked.
- */
-function createFacebookDialog(title, msg, btnConfirmName,
-                               confirmCallback, cancelCallback) {
-   // Creates the confirm component.
-   var lblConfirm = $('<label class="uiButton uiButtonLarge uiButtonConfirm">');   
-   $('<input>', {
-      type: 'button',
-      val: btnConfirmName,
-      click: confirmCallback
-   }).appendTo(lblConfirm);
-   
-   // Creates the cancel component.
-   var lblCancel = $('<label class="uiButton uiButtonLarge">');
-   $('<input>', {
-      type: 'button',
-      val: 'Cancel',
-      click: cancelCallback
-   }).appendTo(lblCancel);
-   
-   // Wrap the buttons to a container.
-   var dialogButtons = $('<div class="dialog_buttons clearfix">');
-   lblConfirm.appendTo(dialogButtons);
-   lblCancel.appendTo(dialogButtons);
-   
-   // Main contents for the dialog description.
-   var dialogContent = $('<div class="dialog_content">');
-   $('<div class="dialog_body">' +
-     '  <form id="report_form" onsubmit="return false;">' +
-     '    <div class="pam uiBoxWhite noborder">' +
-     '      <div class="pbm">' +
-     '        ' + msg +
-     '      </div>' +
-     '    </div>' +
-     '  </form>' +
-     '</div>').appendTo(dialogContent);
-   dialogButtons.appendTo(dialogContent);
-   
-   // Add the title to the the dialog and the contents as well.
-   var dialog = $('<div class="pop_container_advanced">');
-   var dialogPopup = $('<div class="pop_content" id="pop_content">');
-   $('<h2 class="dialog_title"><span>' + title + '</span></h2>').appendTo(dialogPopup);
-   dialogContent.appendTo(dialogPopup);
-   dialogPopup.appendTo(dialog);
-   return dialog;
-}
-
-function goToFriendPageAndStart() {
-  // See if we are at the right page to start.  We need to be at the /friends/*
-  // location, to get access to the list of friends.  Any other page won't do.
-  //
-  // To make sure the user wants to start exporting, we need to prompt her to
-  // see if it's OK to go to the right page (she might have some input in an
-  // input field that is not saved, etc).
-  if (document.location.pathname.match("^/friends/edit") && document.location.search == 0) {
-    switchToWorkerTab();
-  } else {
-    var redirect = document.createElement("div");
-    $(redirect).attr("id", "fb-exporter-redirect")
-               .css("position", "absolute")
-               .css("left", "25%")
-               .css("right", "25%")
-               .css("top", "20%")
-               .html(createFacebookDialog(
-                 'Redirection needed',
-                 'First, you need to go to your friends page',
-                 'Redirect now',
-                 function() {
-                   $("#fb-exporter-redirect").remove();
-                   switchToWorkerTab();
-                   window.location = 'http://www.facebook.com/friends/edit';
-                 },
-                 function() {
-                   $("#fb-exporter-redirect").remove();
-                 })
-               );
-    $(document.body).append(redirect);
-  }
-}
-
-$(document).ready(function() {
-  console.log($("#pageNav  a:contains('Home')").parent());
-
-  var dontCloseBanner = document.createElement("div");
-  $(dontCloseBanner).css("background-color", "pink")
-                    .css("color", "white")
-                    .css("font-size", "18pt")
-                    .css("text-align", "center")
-                    .css("width", "100%")
-                    .addClass("fb-exporter-top-banner")
-                    .text("Don't close this window, please... exporting your data.  ")
-                    .hide();
-  // This first div is just to get the rest of the page content padded
-  // downwards correctly.
-  $(document.body).prepend($(dontCloseBanner));
-
-  // This next dive is to keep the message at the top of the page.  Surely
-  // there's a better way to do this, but meh.
-  var followAlong = document.createElement("a");
-  $(followAlong).text("Care to follow along?")
-                .attr("href", "javascript:void(0);")
-                .click(switchToWorkerTab);
-  $(document.body).prepend(
-      $(dontCloseBanner).clone()
-                        .css("position", "fixed")
-                        .css("z-index", "10000")
-                        .css("top", "0px")
-                        .append($(followAlong)));
-
-  var exportFriendsLink = $("#pageNav a:contains('Home')").parent().clone();
-  $("a", exportFriendsLink)
-      .attr("href", "javascript:void(0);")
-      .text("Export friends!")
-      .css("color", "white")
-      .click(goToFriendPageAndStart);
-  console.log(exportFriendsLink);
-  exportFriendsLink.appendTo($("#pageNav a:contains('Home')").parent());
-
-  chrome.extension.onRequest.addListener(
-    function(request, sender, sendResponse) {
-      console.log(sender.tab ?
-                  "from a content script:" + sender.tab.url :
-                  "from the extension");
-      console.log(request);
-      if (request.getFriendListFromContentScript) {
-        work_tab_id = sender.tab.id;
-
-        var friendNameList = getFriendList();
-        sendResponse({friendNameList: friendNameList});
-      }
-      if (request.pissOffZuckerberg) {
-        startExportFriendData();
-      }
-
-      if (request.hideTopBanner) {
-        hideTopBanner();
-      }
-    });
-  return;
-
-});
