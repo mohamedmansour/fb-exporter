41de6aa8755cc166e49b204be127ea4d1caf7cea
Mohamed Mansour <hello@mohamedmansour.com>
2011-01-11 00:24:14 -0500

    Create some sort of database on the web support to save friend data so we can refer to it prior.

---
+++ js/fb-database.js
@@ -0,0 +1,64 @@
+/**
+ * Storage class responsible for managing the database
+ * tansactions for friends
+ */
+FriendDB = function () {
+  this.db = null;
+};
+
+/**
+ * Opens a connection to Web SQL table.
+ */
+FriendDB.prototype.open = function() {
+  var db_size = 5 * 1024 * 1024; // 5MB
+  this.db = openDatabase('Facebook Friend Export', '1.0', 'fb-export', db_size);
+};
+
+/**
+ * For simplicity, just show an alert when crazy error happens.
+ */
+FriendDB.prototype.onError = function(tx, e) {
+  alert('Something unexpected happened: ' + e.message );
+};
+
+/**
+ * Creats a table with the following columns:
+ *      id   - Integer
+ *      data - String
+ *      ts   - Timestamp
+ */
+FriendDB.prototype.createTable = function() {
+  this.db.transaction(function(tx) {
+    tx.executeSql('CREATE TABLE IF NOT EXISTS ' + 
+                  'friend(id INTEGER, data TEXT, ts DATETIME)', []);
+  });
+};
+
+/**
+ * Adds a |friend| to the database, the contents gets serialized to String.
+ * Current time is tracked as well.
+ */
+FriendDB.prototype.addFriend = function(friend, onSuccess) {
+  var ts = new Date();
+  var data = JSON.stringify(friend);
+  this.db.transaction(function(tx) {
+    tx.executeSql('INSERT INTO friend(id, data, ts) VALUES (?,?,?)', 
+                  [friend.id, data, ts], onSuccess, this.onError);
+  });
+};
+
+/**
+ * Retrieves a row from the table given |id| the result goes to |respawn|. This
+ * is an asynchronous action.
+ */
+FriendDB.prototype.getFriend = function(id, response) {
+  this.db.transaction(function(tx) {
+    tx.executeSql('SELECT data FROM friend WHERE id = ?', [id],
+        function (tx, rs) {
+          if (rs.rows.length != 0) {
+            response(JSON.parse(rs.rows.item(0).data));
+          }
+        }, this.onError
+    );
+  });
+};
