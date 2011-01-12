e2684650ed1c7f18895e0a08bf9aa33affea7e80
Mohamed Mansour <hello@mohamedmansour.com>
2011-01-12 00:21:15 -0500

    CSV Exporter Class implementation

--- js/csv_exporter.js
+++ js/csv_exporter.js
@@ -1,51 +1,46 @@
-// Routines to handle csv exporting.
+/**
+ * Routines to handle csv exporting.
+ */
+CSVExporter = function(friends) {
+  this.friends = friends;
+  this.header = ['Name', 'Email 1', 'Email 2', 'Email 3',
+                  'IM 1', 'IM 2', 'IM 3',
+                  'Website 1', 'Website 2', 'Website 3',
+                  'Website Facebook'
+                 ];
+  this.dump = '';
+};
 
-function csv_DoExport(requestedFriendsToImport) {
-  var csv_header = ["Name", "Email 1", "Email 2", "Email 3",
-                    "IM 1", "IM 2", "IM 3",
-                    "Website 1", "Website 2", "Website 3",
-                    "Website Facebook"
-                    ];
-
+/**
+ * Start processing csv file.
+ * @param {Function<Friend>} callback to fire after each friend processed.
+ */
+CSVExporter.prototype.process = function(callback) {
   var csv_rows = [];
-
-  $.each(requestedFriendsToImport, function(key, friend) {
-    csv_row = [];
+  var that = this;
+  
+  $.each(this.friends, function(key, friend) {
+    var csv_row = [];
     csv_row.push(friend.name);
 
-    for (i = 0; i < 3; i++ ){
-      if (friend.email[i]) {
-        csv_row.push(friend.email[i]);
-      } else {
-        csv_row.push("");
-      }
+    for (i = 0; i < 3; i++){
+      that.addColumn_(csv_row, friend.email[i]);
     }
 
-    for (i = 0; i < 3; i++ ){
-      if (friend.aims[i]) {
-        csv_row.push(friend.aims[i]);
-      } else {
-        csv_row.push("");
-      }
+    for (i = 0; i < 3; i++){
+      that.addColumn_(csv_row, friend.aims[i]);
     }
 
-    for (i = 0; i < 3; i++ ){
-      if (friend.websites[i]) {
-        csv_row.push(friend.websites[i]);
-      } else {
-        csv_row.push("");
-      }
+    for (i = 0; i < 3; i++){
+      that.addColumn_(csv_row, friend.websites[i]);
     }
-
-    if (friend.fb) {
-      csv_row.push(friend.fb);
-    } else {
-      csv_row.push("");
-    }
+    
+    that.addColumn_(csv_row, friend.fb);
 
     csv_rows.push(csv_row);
 
-    chrome.tabs.sendRequest(worker_id, {
+    // Callback to inform client 
+    callback({
         finishedProcessingFriend: true,
         friend: friend,
         success: 1,
@@ -53,11 +48,32 @@ function csv_DoExport(requestedFriendsToImport) {
     });
   });
 
-  var s = csv_header.join(",") + "\n";
+  this.dump = this.header.join(',') + '\n';
 
-  for(i in csv_rows) {
-    s += csv_rows[i].join(",") + "\n";
+  for (i in csv_rows) {
+    this.dump += csv_rows[i].join(',') + '\n';
   }
+};
 
-  return s;
-}
+/**
+ * Get the dump CSV text.
+ */
+CSVExporter.prototype.getDump = function() {
+  return this.dump;
+};
+
+/**
+ * Adds a column safely to each spot.
+ 
+ * @private
+ *
+ * @param {String[]} row A reference to a row that we need to push columns to.
+ * @param {String} column A string that will be added to the row.
+ */
+CSVExporter.prototype.addColumn_ = function(row, column) {
+  if (column) {
+    row.push(column);
+  } else {
+    row.push('');
+  }
+};
